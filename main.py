cbb70167ff192ac6485d70182dcdb30362b86ff0
Dmytro Demchuk <king@MacBook-Pro.local>
2024-05-30 14:15:18 +0300

    third commit

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ async def command_start_handler(message: Message) -> None:
         await message.answer(f"Вітаю *Адміністратору* *{message.from_user.full_name}*!\n\nСюди ти можеш надіслати контент для адмінів 🥰", parse_mode='Markdown')
     else:
         await message.answer(f"Вітаю, *{message.from_user.full_name}*!\n\nСюди ти можеш надіслати контент для адмінів 🥰", parse_mode='Markdown')
-    
+
 @dp.message()
 async def echo_handler(message: Message) -> None:
     try:
@@ -37,16 +37,10 @@ async def echo_handler(message: Message) -> None:
             await bot.forward_message(chat_id=group_id, from_chat_id=message.chat.id, message_id=message.message_id)
             await bot.send_message(chat_id=group_id, text=f"{message.from_user.full_name}" "\nID = " f"{message.from_user.id}")
         await message.reply("Ваше повідомлення було надіслано адміністраторам ❤️", parse_mode='Markdown')
-            
+
     except TypeError:
         await message.answer("Помилка")
 
-@dp.message_handler(lambda message: message.reply_to_message and message.reply_to_message.forward_from)
-async def reply_to_user(message: types.Message):
-    original_message = message.reply_to_message
-    user_id = original_message.forward_from.id
-    
-    await bot.send_message(user_id, message.text)
 
 async def main() -> None:
     bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
